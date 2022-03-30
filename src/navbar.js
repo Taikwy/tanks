14ba5676e9bd4b63b82bf5f7a12beb16c8add7d8
Taikwy <40923736+Taikwy@users.noreply.github.com>
2022-03-30 02:05:13 -0700

    Add files via upload

---
+++ src/navbar.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.navbar');
+
+let about = document.querySelector('.navbar #about');
+let tanks = document.querySelector('.navbar #tanks');
+let credits = document.querySelector('.navbar #credits');
+
+// about.addEventListener('click', () => {about.classList.add('active'); tanks.classList.remove('active'); credits.classList.remove('active');});
+// tanks.addEventListener('click', () => {tanks.classList.add('active'); about.classList.remove('active'); credits.classList.remove('active');});
+// credits.addEventListener('click', () => {credits.classList.add('active'); tanks.classList.remove('active'); about.classList.remove('active');});
